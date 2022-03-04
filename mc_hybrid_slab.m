0eaeedf8e6142579fb773b1fe5541ca4a66aa6f9
ssuleymankahraman <ssuleymankahraman@gmail.com>
2022-03-03 21:46:05 -0800

    Hybrid model v1

--- mc_hybrid_slab.m
+++ mc_hybrid_slab.m
@@ -15,12 +15,23 @@ c           = 299792458e2; % cm/s, speed of light in vacuum
 
 % Scattering medium properties
 n_rel       = 1; % Relative refractive index
-mu_a        = 10; % 1/cm, absorption coefficient
-mu_s        = 90; % 1/cm, absorption coefficient
+mu_a        = 0.1; % 1/cm, absorption coefficient
+mu_s        = 100; % 1/cm, absorption coefficient
+g           = 0.9; % Scattering anisotropy
+d           = 1; % cm, depth of slab
+z_c         = 0.05; % Diffusion theory region distance from boundary
+
+% Derived paramters
 mu_t        = mu_a + mu_s;
-g           = 0.75; % Scattering anisotropy
-l_t_prime   = 1/(mu_a+mu_s*(1-g)); % Transport mean free path
-d           = 0.02; % cm, depth of slab
+mu_s_prime  = mu_s*(1-g);
+mu_t_prime  = mu_a + mu_s_prime; 
+a_prime     = mu_s_prime/mu_t_prime;
+D           = 1/3/(mu_t_prime);
+mu_eff      = sqrt(mu_a/D);
+l_t_prime   = 1/(mu_t_prime); % Transport mean free path
+R_eff = -1.440/n_rel^2+0.710/n_rel+0.668+0.0636*n_rel;
+C_R = (1+R_eff)/(1-R_eff);
+z_b = 2*C_R*D;
 
 % Output flags
 show_vis    = false; % Flag for visualization (plots/figures)
@@ -29,19 +40,19 @@ verbose     = false; % Flag for printing output
 %% Initialize parameters for monte carlo
 % Details of photon packets
 init_wt     = 1e0; % Initial weight of packet
-N_packet    = 1e4; % Number of photon packets
+N_packet    = 1e5; % Number of photon packets
 th_wt       = 1e-4; % Weight threshold below which a photon packet is dead
 
 russ_m = 10; % Russian roulette parameter
 
 % Computational grid for recording output
-dz          = 0.005;%10*lambda/10; % (cm) Grid size along z
-dr          = 0.005;%lambda/10; % (cm) Grid size along r
+dz          = 0.01;%10*lambda/10; % (cm) Grid size along z
+dr          = 0.01;%lambda/10; % (cm) Grid size along r
 dalpha      = pi/180; % Grid size along alpha (angle)
 
 % TBD: Define mesh
-Nz = 200;
-Nr = 10;
+Nz = ceil(d/dz);
+Nr = 200;
 z = ([0:Nz-1]+1/2)*dz;
 r = (([0:Nr-1]+1/2)+1/12./([0:Nr-1]+1/2))*dr;
 
@@ -52,6 +63,7 @@ Da = 2*pi*([0:Nr-1]+1/2)*dr^2;
 DOmega = 4*pi*sin(([0:Nalpha-1]+1/2)*dalpha)*sin(dalpha/2);
 
 A_rz = zeros(Nr,Nz);
+S_rz = zeros(Nr,Nz);
 R_ralpha = zeros(Nr,Nalpha);
 T_ralpha = zeros(Nr,Nalpha);
 
@@ -75,6 +87,7 @@ parfor n = 1:N_packet
     % For parfor to be able to split
     photon = photon_data(n,:);
     A_rz_temp = zeros(Nr,Nz);
+    S_rz_temp = zeros(Nr,Nz);
     R_ralpha_temp = zeros(Nr,Nalpha);
     T_ralpha_temp = zeros(Nr,Nalpha);
     
@@ -202,13 +215,23 @@ parfor n = 1:N_packet
         % Is the photon in the center slice, and travelling in the downward
         % direction? If yes, move the photon by l_t_prime and use Diffusion
         % theory.
-        if ((photon(1,3) > l_t_prime) && (photon(1,3) < d - l_t_prime) && (photon(1,6) > 0))
-            photon(1,1:3) = photon(1,1:3) + l_t_prime/mu_t * photon(1,4:6);
-            
+        if ((photon(1,3) > z_c) && (photon(1,3) < d - z_c) ) % && (photon(1,6) > 0)
+            next_position = photon(1,1:3) + l_t_prime * photon(1,4:6);
+            if ((next_position(1,3) > z_c) && (next_position(1,3) < d - z_c) ) % && (photon(1,6) > 0)
+                
             % Use diffusion theory to simulate an isotropic source now
             % TBD
+            [~,i_r] = min(abs(sqrt(photon(1,1)^2+photon(1,2)^2)-r));
+            [~,i_z] = min(abs(photon(1,3)-z));
+%             alpha_packet = atan2(photon(1,2),photon(1,1));
+%             alpha_packet(alpha_packet<0) = alpha_packet(alpha_packet<0) + 2*pi;
+%             [~,i_alpha] = min(abs(alpha_packet-alpha));
+                
+            S_rz_temp(i_r,i_z) = S_rz_temp(i_r,i_z) + photon(1,7);
+            photon(1,7) = 0;
             
             photon(1,8) = 1; % Kill photon packet
+            end
         end
         
         if ((photon(1,8) == 0)&&(photon(1,7) > th_wt)) % Photon alive & Weight large enough
@@ -228,6 +251,7 @@ parfor n = 1:N_packet
         end
     end
     A_rz = A_rz + A_rz_temp;
+    S_rz = S_rz + S_rz_temp;
     R_ralpha = R_ralpha + R_ralpha_temp;
     T_ralpha = T_ralpha + T_ralpha_temp;
     photon_data(n,:) = photon;
@@ -235,12 +259,6 @@ end
 %% Output recorded quantities
 
 toc
-
-A_z = sum(A_rz,1);
-A = sum(A_rz,[1,2]);
-A_rz = A_rz./(N_packet*Da'*dz);
-A_z = A_z/(N_packet*dz);
-A = (A/N_packet) *(1-((n_rel-1)/(n_rel+1))^2);
 
 
 R_alpha = sum(R_ralpha,1);
@@ -252,23 +270,50 @@ R_r = R_r./(N_packet*Da');
 R = R/N_packet;
 
 
-T_alpha = sum(T_ralpha,1);
-T_r = sum(T_ralpha,2);
-T = sum(T_ralpha,[1,2]);
-T_ralpha = T_ralpha./(N_packet*Da'.*DOmega.*cos(alpha));
-T_alpha = T_alpha./(N_packet*DOmega);
-T_r = T_r./(N_packet*Da');
-T = T/N_packet;
 
-R_diff = R;
-R = R*(1-((n_rel-1)/(n_rel+1))^2) + ((n_rel-1)/(n_rel+1))^2;
+Sd_rz = S_rz./(N_packet*Da'*dz);
 
-F_z = A_z / mu_a;
+dphi = 0.1;
+phi = reshape([0:dphi:pi],1,1,[]);
 
-% figure;
-% semilogy(z,F_z);
-% xlabel('z (cm)'); ylabel('Fluence'); 
-% hold on;
-% xline(l_t_prime,'--');
+for i = 1:length(r)
+    Rd = zeros(Nr,Nz,length(phi));
+for j = -1:1
+    z1 = -z_b + 2*j*(d+2*z_b) + (z+z_b);
+    z2 = -z_b + 2*j*(d+2*z_b) - (z+z_b);
+    rho1 = sqrt(r(i).^2 + r'.^2 - 2*r(i)*r'.*cos(phi) + z1.^2);
+    rho2 = sqrt(r(i).^2 + r'.^2 - 2*r(i)*r'.*cos(phi) + z2.^2);
+    Rd = Rd + z1.*(1+mu_eff*rho1).*exp(-mu_eff*rho1)./(4*pi*rho1.^3);
+    Rd = Rd - z2.*(1+mu_eff*rho2).*exp(-mu_eff*rho2)./(4*pi*rho2.^3);
+end
+    R_DT(i,1) = 2*sum(Sd_rz.*r'.*Rd,[1 2 3])*dr*dz*dphi;
+end
 
-% imagesc(log(A_rz));
+figure;
+semilogy(r,R_DT); hold; 
+semilogy(r,R_DT+R_r);
+xlabel('r (cm)');
+legend('R_{DT}(r)','R_{DT}(r)+R_{MC}(r)'); 
+
+% A_z = sum(A_rz,1);
+% A = sum(A_rz,[1,2]);
+% A_rz = A_rz./(N_packet*Da'*dz);
+% A_z = A_z/(N_packet*dz);
+% A = (A/N_packet) *(1-((n_rel-1)/(n_rel+1))^2);
+% 
+% 
+% 
+% 
+% T_alpha = sum(T_ralpha,1);
+% T_r = sum(T_ralpha,2);
+% T = sum(T_ralpha,[1,2]);
+% T_ralpha = T_ralpha./(N_packet*Da'.*DOmega.*cos(alpha));
+% T_alpha = T_alpha./(N_packet*DOmega);
+% T_r = T_r./(N_packet*Da');
+% T = T/N_packet;
+% 
+% R_diff = R;
+% R = R*(1-((n_rel-1)/(n_rel+1))^2) + ((n_rel-1)/(n_rel+1))^2;
+% 
+% F_z = A_z / mu_a;
+
